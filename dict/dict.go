7bd81bc1327c5beee898539d41dc9ff3e03173d8
limes22 <howdi2002@naver.com>
2022-12-28 15:05:59 +0900

    go lesson 01

---
+++ dict/dict.go
@@ -0,0 +1,55 @@
+package dict
+
+import "errors"
+
+// Dictionary type
+type Dictionary map[string]string
+
+var (
+	errNotFound    = errors.New("Not Found")
+	errCantUpdate  = errors.New("Cant update non-existing word")
+	errWordExisits = errors.New("That word already exists")
+)
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExisits
+	}
+	//if err == errNotFound {
+	//	d[word] = def
+	//} else if err == nil {
+	//	return errWordExisits
+	//}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
