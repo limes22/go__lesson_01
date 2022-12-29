b8273b225b46d8b201e44c02d0a418167a928f6a
limes22 <howdi2002@naver.com>
2022-12-29 15:27:28 +0900

    go lesson

--- main.go
+++ main.go
@@ -1,49 +1,33 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
+//Goroutines : 기본적으로 다른 함수와 동시에 실행시키는 함수
+//Channel : goroutine 이랑 메인함수 사이에 정보를 전달하기 위한 방법
 
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	c := make(chan string)
+	people := [5]string{"sue", "jin", "oh", "j", "h"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	//results["gello"] = "Hello"
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-// 웹사이트로 접속(hit)하고 그 결과를 알려주는 함수
-// (*hit: 인터넷 웹 서버의 파일 1개에 접속하는 것을 뜻함)
+//func sexyCount(person string) {
+//	for i := 0; i < 10; i++ {
+//		fmt.Println(person, "is sexy", i)
+//		time.Sleep(time.Second)
+//	}
+//}
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	fmt.Println(person)
+	c <- person + " is sexy"
 }
---
+++ main.url
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	//results["gello"] = "Hello"
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+// 웹사이트로 접속(hit)하고 그 결과를 알려주는 함수
+// (*hit: 인터넷 웹 서버의 파일 1개에 접속하는 것을 뜻함)
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
